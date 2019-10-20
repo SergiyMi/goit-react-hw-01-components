a1d13185133a470c3c69fad4ddea17844bc6ce50
SergiyMi <bladeneo@i.ua>
2019-10-20 20:13:52 +0300

    use react

--- src/component/FriendList/FriendListItem.jsx
+++ src/component/FriendList/FriendListItem.jsx
@@ -3,16 +3,15 @@ import React from 'react';
 import styles from './FriendListItem.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline, friends }) => {
-    let onOf;
     return (
         friends.map((friend, index) => (
-            (isOnline[index] ? onOf=styles.isOn : onOf=styles.isOff),
-            <li className={styles.item}>
-                <span className={onOf + ' ' + styles.circle}></span>
+                <li className={styles.item}>
+                <span className={isOnline[index] ? styles.isOn : styles.isOff + ' ' + styles.circle}></span>
                 <img className={styles.avatar} src={avatar[index]} alt=""/>
                 <p className={styles.name}>{name[index]}</p>
             </li>
-        ))
+            )
+        )
     )
 }
 
