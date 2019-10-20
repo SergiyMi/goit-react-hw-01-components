78b2e2d5f8de605cdfab75b7ff774443bd9901d5
SergiyMi <bladeneo@i.ua>
2019-10-20 20:30:02 +0300

    Updates

---
+++ precache-manifest.4798e78e27e149b29f7f39da6f2e87be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d557deb2098f682bd788ee8781054e3d",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "89067b808b6dcb5613f7",
+    "url": "/goit-react-hw-01-components/static/css/main.2ff67a3e.chunk.css"
+  },
+  {
+    "revision": "242fdb978a28c30ac419",
+    "url": "/goit-react-hw-01-components/static/js/2.c4dbaa65.chunk.js"
+  },
+  {
+    "revision": "89067b808b6dcb5613f7",
+    "url": "/goit-react-hw-01-components/static/js/main.42c8a1c2.chunk.js"
+  },
+  {
+    "revision": "1deac508ae55ff559187",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.a749bb14.js"
+  }
+]);
